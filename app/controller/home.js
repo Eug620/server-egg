9680d3b68c31be412a82b75e2f42085db7c3abd5
yeyuhang <yeyuhang@appicplay.com>
2022-03-10 19:57:43 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2022-03-08 14:50:53
- * @LastEditTime : 2022-03-10 17:02:05
+ * @LastEditTime : 2022-03-10 19:44:06
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /server-egg/app/controller/home.js
@@ -13,10 +13,14 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx, app: { controller } } = this;
+    const renderList = {}
+    for (const key in controller) {
+      renderList[key] = Object.keys(controller[key]).map(v => `/${key}/${v}`)
+    }
     await ctx.render('index.njk', {
       userName: '张三',
       content: '这是传入的内容',
-      list: Object.keys(controller)
+      list: renderList
     })
   }
 }
