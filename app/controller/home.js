93c2764c19bc657f8dbff78e245651a94dc94890
yeyuhang <yeyuhang@appicplay.com>
2022-03-11 14:24:16 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2022-03-08 14:50:53
- * @LastEditTime : 2022-03-10 19:44:06
+ * @LastEditTime : 2022-03-11 14:23:49
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /server-egg/app/controller/home.js
@@ -18,8 +18,6 @@ class HomeController extends Controller {
       renderList[key] = Object.keys(controller[key]).map(v => `/${key}/${v}`)
     }
     await ctx.render('index.njk', {
-      userName: '张三',
-      content: '这是传入的内容',
       list: renderList
     })
   }
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,11 +1,26 @@
 /* 
  * @Author       : Eug
  * @Date         : 2022-03-10 15:07:25
- * @LastEditTime : 2022-03-10 15:10:21
+ * @LastEditTime : 2022-03-11 14:23:04
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /server-egg/app/public/js/index.js
  */
 const box = document.querySelector('.banner-wrapper')
-console.log(box);
-box.style.backgroundColor = '#ccc'
+if (box) {
+  box.style.backgroundColor = '#ccc'
+}
+
+const handleClick = (e) => {
+  const buttons = document.querySelectorAll('.headerButton')
+  buttons.forEach(b => {
+    if (e.textContent === b.textContent) {
+      b.classList.remove('outline')
+    } else {
+      if (!b.getAttribute('class').includes('outline')){
+        b.classList.add('outline')
+      }
+    }
+    
+  })
+}
