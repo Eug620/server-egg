aee0029e592dd9ddc4ab68c6e1f9889ade64aa26
eug <yyh3531@163.com>
2022-08-27 05:04:41 +0800

    端口

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2022-02-11 14:42:57
- * @LastEditTime: 2022-08-27 04:46:25
+ * @LastEditTime: 2022-08-27 05:04:08
  * @LastEditors: eug yyh3531@163.com
  * @Descripttion : Descripttion
  * @FilePath     : /server-egg/config/config.default.js
@@ -50,7 +50,7 @@ module.exports = appInfo => {
       enable: false,
       ignoreJSON: true,
     },
-    domainWhiteList: ['http://www.baidu.com', 'http://47.93.229.170'], // 配置白名单
+    domainWhiteList: ['http://www.baidu.com', 'http://47.93.229.170:80'], // 配置白名单
   }
   config.cors = {
     // origin: '*',//允许所有跨域访问，注释掉则允许上面 白名单 访问
