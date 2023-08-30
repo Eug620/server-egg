cfba324b1e5656b994f80d0c71b186bfbec18776
eug <yyh3531@163.com>
2023-08-30 14:30:49 +0800

    fix(schedule): type

--- app/schedule/assets.js
+++ app/schedule/assets.js
@@ -1,7 +1,7 @@
 module.exports = {
     schedule: {
         interval: '1s', // 1 分钟间隔
-        // type: 'all', // 指定所有的 worker 都需要执行
+        type: 'all', // 指定所有的 worker 都需要执行
     },
     async task(ctx) {
         console.log('schedule: hhhhh 1s');
